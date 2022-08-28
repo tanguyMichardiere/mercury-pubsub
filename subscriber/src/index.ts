30f77d82f4fc476118dcdc3690d13231a5be4b56
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2022-08-28 21:26:57 +0200

    update subscriber

--- subscriber/src/index.ts
+++ subscriber/src/index.ts
@@ -9,25 +9,25 @@ export type SubscribeOptions<C extends keyof Channels> = Pick<
 > & { ondata: (data: Channels[C]) => void };
 
 export default class Subscriber {
-  private url: string;
-  private key: string;
+  #url: string;
+  #key: string;
 
   constructor(url: string, key: string) {
-    this.url = url;
-    this.key = key;
+    this.#url = url;
+    this.#key = key;
   }
 
   async subscribe<C extends keyof Channels>(
     channel: C,
     { signal, onopen, ondata, onclose }: SubscribeOptions<C>
   ): Promise<void> {
-    const url = new URL(`/sse/${channel}`, this.url);
+    const url = new URL(`/sse/${channel}`, this.#url);
     function onmessage(ev: EventSourceMessage) {
       ondata(JSON.parse(ev.data));
     }
     await fetchEventSource(url.href, {
       signal,
-      headers: { Authorization: `Bearer ${this.key}` },
+      headers: { Authorization: `Bearer ${this.#key}` },
       onopen,
       onmessage,
       onclose,
--- subscriber/src/react.tsx
+++ subscriber/src/react.tsx
@@ -1,9 +1,9 @@
-import { createContext, useRef } from "react";
+import { createContext, useContext, useEffect, useRef } from "react";
 
 import type { Channels, SubscribeOptions } from ".";
 import Subscriber from ".";
 
-const subscriberContext = createContext<Subscriber>(undefined as unknown as Subscriber);
+const subscriberContext = createContext<Subscriber | undefined>(undefined);
 
 export default function SubscriberProvider(props: { url: string; key: string }): JSX.Element {
   const subscriber = useRef(new Subscriber(props.url, props.key));
@@ -11,7 +11,26 @@ export default function SubscriberProvider(props: { url: string; key: string }):
   return <subscriberContext.Provider value={subscriber.current}></subscriberContext.Provider>;
 }
 
-export function useSubscribe<C extends Channels>(
+export function useSubscribe<C extends keyof Channels>(
   channel: C,
   { onopen, ondata, onclose }: SubscribeOptions<C>
-) {}
+): void {
+  const subscriber = useContext(subscriberContext);
+
+  if (subscriber === undefined) {
+    throw new Error("no subscriber context");
+  }
+
+  const abortController = useRef(new AbortController()).current;
+
+  useEffect(function () {
+    subscriber
+      .subscribe(channel, { signal: abortController.signal, onopen, ondata, onclose })
+      .catch(function (error) {
+        console.error(error);
+      });
+    return function () {
+      abortController.abort();
+    };
+  });
+}
