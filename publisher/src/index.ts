b0dbe11c11cef72c5b51d4d60d31cad71a05a5c8
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2022-08-28 21:26:51 +0200

    update publisher

--- publisher/src/index.ts
+++ publisher/src/index.ts
@@ -1,22 +1,22 @@
-import fetch from "node-fetch";
+import "whatwg-fetch";
 
 type Channels = Record<string, unknown>;
 
 export default class Publisher {
-  private url: string;
-  private key: string;
+  #url: string;
+  #key: string;
 
   constructor(url: string, key: string) {
-    this.url = url;
-    this.key = key;
+    this.#url = url;
+    this.#key = key;
   }
 
   async publish<C extends keyof Channels>(channel: C, data: Channels[C]): Promise<number> {
-    const url = new URL(`/sse/${channel}`, this.url);
+    const url = new URL(`/sse/${channel}`, this.#url);
     const response = await fetch(url.href, {
       method: "POST",
       body: JSON.stringify(data),
-      headers: { Authorization: `Bearer ${this.key}` },
+      headers: { Authorization: `Bearer ${this.#key}` },
     });
     if (!response.ok) throw new Error(await response.text());
     return parseInt(await response.text());
