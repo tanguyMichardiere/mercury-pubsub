ce162554721f77c366fdd98739c0fe127d6f4a67
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2022-09-01 23:54:26 +0200

    use vercel edge runtime

--- examples/single-channel-chat/src/pages/api/send.ts
+++ examples/single-channel-chat/src/pages/api/send.ts
@@ -1,14 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
 import Publisher from "@mercury-pubsub/publisher";
+
+export const config = {
+  runtime: "experimental-edge",
+};
 
 const publisher = new Publisher(
   process.env.NEXT_PUBLIC_MERCURY_URL,
   process.env.MERCURY_PUBLISHER_KEY
 );
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<number>) {
-  const count = await publisher.publish("messages", req.body);
-  res.status(200).json(count);
+export default async function handler(req: NextRequest) {
+  const count = await publisher.publish("messages", await req.text());
+  return new Response(count.toString());
 }
